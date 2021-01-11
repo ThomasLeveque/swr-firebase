293682ce51bfd2c8aa8d5c16c87c751db060a469
ThomasLeveque <thomasl5895@gmail.com>
2021-01-11 11:58:58 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/about.js
+++ pages/about.js
@@ -2,21 +2,20 @@ import Head from 'next/head';
 import useSWR from 'swr';
 import { fetchTotos } from '../lib/fetchers';
 
-export default function Home() {
+export default function About() {
   const { data: totos } = useSWR(
-    ['totos', JSON.stringify({ orderBy: ['name', 'asc'] })],
+    ['totos', JSON.stringify({ where: ['value', '==', 1] })],
     fetchTotos
   );
-
   return (
     <div>
       <Head>
-        <title>SWR Firebase</title>
+        <title>SWR Firebase - About</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>Coucou</h1>
+        <h1>About</h1>
         {!totos ? (
           <p>Loading...</p>
         ) : (
